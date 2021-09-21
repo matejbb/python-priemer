c51f206ee1a09e81c3293196d5b958f7c668d629
matejbb <87907823+matejbb@users.noreply.github.com>
2021-09-21 09:04:17 +0200

    Add files via upload

--- priemer.py
+++ priemer.py
@@ -1,13 +1,4 @@
-y = True
-pocet = 0
-sucet = 0
-while y:
-    x = input("Zadaj číslo: ")
-    if (x == "koniec"):
-        priemer = sucet/pocet
-        print("Priemer je ",priemer)
-        y = False
-    else:
-        sucet = sucet + int(x)
-        pocet = pocet + 1
-        
+x = int(input("Zadaj prvé číslo: "))
+y = int(input("Zadaj druhé číslo: "))
+z = int(input("Zadaj tretie číslo: "))
+print("Tvoj priemer je" ,(x+y+z)/3)
